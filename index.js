893400c251a623239504a01b02766823e31e98ed
Ryan White <mrwhiterk@gmail.com>
2019-07-18 00:38:52 -0400

    got new comment form working and added button on show page

--- index.js
+++ index.js
@@ -86,6 +86,29 @@ app.get('/campgrounds/:id/comments/new', (req, res) => {
   });
 });
 
+app.post('/campgrounds/:id/comments', (req, res) => {
+  // lookup campground using ID
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      // create new comment
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err);
+        } else {
+          // connect new comment to campground
+          campground.comments.push(comment);
+          campground.save();
+          // redirect camground show page
+          res.redirect('/campgrounds/' + campground._id);
+        }
+      });
+    }
+  });
+});
+
 app.listen(3000, () => {
   console.log('app listening on port 3000!');
 });
