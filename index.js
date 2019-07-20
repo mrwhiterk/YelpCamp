22fc0147b4e48d2b0fbcf61a7440aeceedabea57
Ryan White <mrwhiterk@gmail.com>
2019-07-20 12:56:34 -0400

    added author reference to comment model to dynamically display author when comment is posted

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-seedDB();
+// seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(
